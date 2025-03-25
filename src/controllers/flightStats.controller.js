a913497924193dbf8168de4093ec1f68d940c5f6
Alexandra Vegas <alexvegasdev@gmail.com>
2025-03-24 22:21:31 -0500

    refactor: :zap: update flight stats

--- src/controllers/flightStats.controller.js
+++ src/controllers/flightStats.controller.js
@@ -1,21 +1,22 @@
-const { QueryTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { QueryTypes } = require("sequelize");
+const sequelize = require("../config/database");
 
 exports.getFlightCounts = async (req, res) => {
   try {
     const flights = await sequelize.query(
-      `SELECT destination_airport_id, COUNT(id) AS flight_count 
-       FROM flights 
-       GROUP BY destination_airport_id`,
+      `SELECT a.name AS airport_name, a.country, COUNT(f.id) AS flight_count 
+       FROM flights f
+       JOIN airports a ON f.destination_airport_id = a.id
+       GROUP BY a.id, a.name, a.country`,
       { type: QueryTypes.SELECT }
     );
 
     res.json(flights);
   } catch (error) {
-    console.error('🔥 ERROR en getFlightCounts:', error);
-    res.status(500).json({ 
-      error: 'Error fetching flight count', 
-      details: error.message 
+    console.error("🔥 ERROR en getFlightCounts:", error);
+    res.status(500).json({
+      error: "Error fetching flight count",
+      details: error.message,
     });
   }
 };
